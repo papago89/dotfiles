709da903f412ad85059a4c6a2281c9f2cf0c30ab
papago89 <papago2683@gmail.com>
2025-07-02 16:02:10 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 DOTFILES_DIR=~/dotfiles
 
+#install zsh
+#curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+#install nvim
+
 ln -sf $DOTFILES_DIR/bashrc ~/.bashrc
 ln -sf $DOTFILES_DIR/zshrc ~/.zshrc
 ln -sf $DOTFILES_DIR/nvim ~/.config/nvim
